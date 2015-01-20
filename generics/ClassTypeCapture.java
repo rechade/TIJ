cdd7c0f968a0f339bee66cbc9e11b5c3f99141b5
rechade <swPf4JsBz3N1AgUuvgsk>
2015-01-20 07:07:50 +0000

    generics up to ex21
    continuing this chapter

--- generics/ClassTypeCapture.java
+++ generics/ClassTypeCapture.java
@@ -1,10 +1,30 @@
+// TIJ generics ex21 p472
+u
 //: generics/ClassTypeCapture.java
-
+import java.util.*;
+import java.lang.reflect.*;
 class Building {}
 class House extends Building {}
 
 public class ClassTypeCapture<T> {
 	Class<T> kind;
+	Map<String,Class<?>> typeMap = new HashMap<String,Class<?>>();
+	
+	public void addType(String typeName, Class<?> kind) {
+		typeMap.put(typeName,kind);
+	}
+	public Object createNew (String typeName) {
+		Object result = null;
+		Class<?> classType = typeMap.get(typeName);
+		try {
+			result = classType.newInstance();
+		} catch (Exception e) {
+			//e.printStackTrace();
+			System.out.println("Error - unable to make "+typeName);
+			System.exit(1);
+		}
+		return result;
+	}
 	public ClassTypeCapture(Class<T> kind) {
 		this.kind = kind;
 	}
@@ -20,6 +40,11 @@ public class ClassTypeCapture<T> {
 			new ClassTypeCapture<House>(House.class);
 		System.out.println(ctt2.f(new Building()));
 		System.out.println(ctt2.f(new House()));
+		ctt2.addType("House", House.class);		
+		ctt2.addType("Building", Building.class);		
+		System.out.println(ctt2.createNew("House"));	
+		System.out.println(ctt2.createNew("Building"));
+		System.out.println(ctt2.createNew("Block"));
 	}
 } /* Output:
 true
